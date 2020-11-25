339235d28e57e862c19bbf0acf87cef802433d01
Ali <ali.rizvi1@gmail.com>
2020-11-25 14:08:24 -0500

    Add 2 queries for q5

--- assignment 3/sql/ex5.sql
+++ assignment 3/sql/ex5.sql
@@ -14,3 +14,16 @@ FROM admissionticket a
 JOIN museum m on m.museumNo = a.museumNo
 GROUP BY m.museumNo
 HAVING COUNT(DISTINCT a.visitorNo)>=10;
+
+-- Find the name of curators (if they exist) who are in charge of expositions with start dates before Jan 01 2016
+SELECT name
+FROM curator
+WHERE EXISTS 
+(SELECT curatorNo 
+	FROM Exposition 
+	WHERE curator.curatorNo = Exposition.curatorNo AND startDate < '2016-01-01');
+
+-- Visitor's who have first names beginning with A and last names ending with z
+SELECT name
+FROM visitor
+WHERE name LIKE 'A%z';
