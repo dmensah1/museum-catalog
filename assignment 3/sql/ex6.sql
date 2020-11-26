186c0bb423416926f7ad32e959b9867d604c2e8e
Ali <ali.rizvi1@gmail.com>
2020-11-26 12:51:00 -0500

    formatting changes

--- assignment 3/sql/ex6.sql
+++ assignment 3/sql/ex6.sql
@@ -6,18 +6,16 @@ SELECT current_date(), v.visitorNo, 'Ancient Clothes'
 FROM visitor v
 WHERE v.name = 'Aitana Savage';
 
-
--- update all artifact themes with 'Rocks' to 'Geology'
-UPDATE theme
-SET artifactTheme = 'Geology'
-WHERE artifactTheme = 'Rocks';
-
-
 -- see artifacts
 SELECT f.*, v.name, v.email
 FROM favoritedetails f
 JOIN visitor v on v.visitorNo = f.visitorNo
 WHERE v.visitorNo = 75;
+
+-- update all artifact themes with 'Rocks' to 'Geology'
+UPDATE theme
+SET artifactTheme = 'Geology'
+WHERE artifactTheme = 'Rocks';
 
 -- delete favoriteDetails that were added before beginning of February 2015
 DELETE FROM favoritedetails
